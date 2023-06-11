d90a757cdf96ef88db128382f06210b64796f6e1
Hiroaki Nagasaka <hiroaki.nagasaka37@gmail.com>
2023-06-11 20:50:32 +0900

    サンプル作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import { ReactComponent as Test } from "./test.drawio.svg";
+import { reactiveSVGSettings, loadSettings } from "@nagasakah/reactive-svg";
 
+const settings: reactiveSVGSettings = {
+  baseURL: `${process.env.PUBLIC_URL}md/`,
+  events: [
+    {
+      name: "rect1",
+      markdownFile: "helloworld.md",
+    },
+  ],
+};
 function App() {
+  const ref = useRef<SVGSVGElement>(null);
+  useEffect(() => {
+    if (ref.current) {
+      loadSettings(settings, ref.current);
+    }
+  }, [ref]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test ref={ref} />
     </div>
   );
 }
